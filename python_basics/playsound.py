73b66f6cd6343b21449b167d279100210de8042c
Lars Bijleveld <lars.bijleveld@gmail.com>
2018-09-13 21:32:39 +0200

    imported simpleaudio. added assignment Python sample player.

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,5 +1,10 @@
 import simpleaudio as sa
+i = 0
 
-wave_obj = sa.WaveObject.from_wave_file("vinyl1.wav")
-play_obj = wave_obj.play()
-play_obj.wait_done()
+insert = input("Please tell me how many times you would like to hear the vinyl ripple. ")
+insert = int(insert)
+while( i < insert):
+  wave_obj = sa.WaveObject.from_wave_file("vinyl1.wav")
+  play_obj = wave_obj.play()
+  play_obj.wait_done()
+  i += 1
