f4c320eca3f951d8b67ae80405c01b1a8b94aa7f
Lars Bijleveld <lars.bijleveld@gmail.com>
2018-09-20 21:44:07 +0200

    added several features, added comments.

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,10 +1,40 @@
+#import modules
 import simpleaudio as sa
-i = 0
+import time
+from random import randint
 
-insert = input("Please tell me how many times you would like to hear the vinyl ripple. ")
-insert = int(insert)
-while( i < insert):
-  wave_obj = sa.WaveObject.from_wave_file("vinyl1.wav")
-  play_obj = wave_obj.play()
-  play_obj.wait_done()
-  i += 1
+#array with rhythm possibilities
+rhythm = [0.125, 0.25, 0.5, 1, 2, 4]
+
+#random bpm count between 60 and 180
+bpm = randint(20, 40)
+
+#empty list
+sequence = []
+x = 0
+
+#input from user
+numPlaybackTimes = input("Gimme some RHYTHM! ")
+numPlaybackTimes = int(numPlaybackTimes)
+
+#create list with random rhythm values
+for i in range(0, numPlaybackTimes):
+    sequence.append(rhythm[randint(0, 5)])
+print(sequence)
+print(bpm)
+
+#forloop with scrolling "sequence" trough "bpm" with playing sound file.
+while (x < bpm):
+    time.sleep(sequence[0])
+    bpm = bpm - sequence[0]
+    sequence.append(sequence[0])
+    del(sequence[0])
+    wave_obj = sa.WaveObject.from_wave_file("noise1.wav")
+    play_obj = wave_obj.play()
+    print(bpm)
+#if statement to reintiate loop with new bpm value
+    if bpm <= 0:
+        bpm = randint(20, 40)
+        sequence.append(rhythm[randint(0, 5)])
+        print(sequence)
+        print(bpm)
